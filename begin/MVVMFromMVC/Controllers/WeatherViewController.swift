8602e660b3543b15017860b95de1aae16bcf92be
Juno Yu <jxy35@hitit.xyz>
2020-07-03 11:32:17 +0900

    Data binding using box

--- begin/MVVMFromMVC/Controllers/WeatherViewController.swift
+++ begin/MVVMFromMVC/Controllers/WeatherViewController.swift
@@ -29,18 +29,7 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
-  private let geocoder = LocationGeocoder()
-  private let defaultAddress = "McGaheysville, VA"
-  private let dateFormatter: DateFormatter = {
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "EEEE, MMM d"
-    return dateFormatter
-  }()
-  private let tempFormatter: NumberFormatter = {
-    let tempFormatter = NumberFormatter()
-    tempFormatter.numberStyle = .none
-    return tempFormatter
-  }()
+	private let viewModel = WeatherViewModel()
   
   @IBOutlet weak var cityLabel: UILabel!
   @IBOutlet weak var dateLabel: UILabel!
@@ -49,38 +38,24 @@ class WeatherViewController: UIViewController {
   @IBOutlet weak var forecastSummary: UITextView!
   
   override func viewDidLoad() {
-    geocoder.geocode(addressString: defaultAddress) { [weak self] locations in
-      guard
-        let self = self,
-        let location = locations.first
-        else {
-          return
-        }
-      self.cityLabel.text = location.name
-      self.fetchWeatherForLocation(location)
-    }
-  }
-
-  func fetchWeatherForLocation(_ location: Location) {
-    //1
-    WeatherbitService.weatherDataForLocation(
-      latitude: location.latitude,
-      longitude: location.longitude) { [weak self] (weatherData, error) in
-      //2
-      guard
-        let self = self,
-        let weatherData = weatherData
-        else {
-          return
-        }
-      self.dateLabel.text =
-        self.dateFormatter.string(from: weatherData.date)
-      self.currentIcon.image = UIImage(named: weatherData.iconName)
-      let temp = self.tempFormatter.string(
-        from: weatherData.currentTemp as NSNumber) ?? ""
-      self.currentSummaryLabel.text =
-        "\(weatherData.description) - \(temp)℉"
-      self.forecastSummary.text = "\nSummary: \(weatherData.description)"
-    }
-  }
+		viewModel.locationName.bind { [weak self] locationName in
+			self?.cityLabel.text = locationName
+		}
+		
+		viewModel.date.bind { [weak self] date in
+			self?.dateLabel.text = date
+		}
+		
+		viewModel.icon.bind { [weak self] image in
+			self?.currentIcon.image = image
+		}
+		
+		viewModel.summary.bind { [weak self] summary in
+			self?.currentSummaryLabel.text = summary
+		}
+		
+		viewModel.forecastSummary.bind { [weak self] forecast in
+			self?.forecastSummary.text = forecast
+		}
+	}
 }
---
+++ begin/MVVMFromMVC/View Models/WeatherViewModel.swift
@@ -0,0 +1,100 @@
+/// Copyright (c) 2020 Razeware LLC
+/// 
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+/// 
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+/// 
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+/// 
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+//1. First, add an import for UIKit.UIImage. No other UIKit types need to be permitted in the view model. A general rule of thumb is to never import UIKit in your view models.
+import UIKit.UIImage
+
+//2. Then, set WeatherViewModel‘s class modifier to public. You make it public in order for it to be accessible for testing.
+public class WeatherViewModel {
+  static private let defaultAddress = "Atlanta, GA"
+  private let geocoder = LocationGeocoder()
+  private let dateFormatter: DateFormatter = {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "EEEE, MMM d"
+    return dateFormatter
+  }()
+  private let tempFormatter: NumberFormatter = {
+    let tempFormatter = NumberFormatter()
+    tempFormatter.numberStyle = .none
+    return tempFormatter
+  }()
+	let locationName = Box("Loading...")
+	let date = Box(" ")
+	let icon:Box<UIImage?> = Box(nil)
+	let summary = Box(" ")
+	let forecastSummary = Box(" ")
+	
+	init() {
+		changeLocation(to: Self.defaultAddress)
+	}
+	
+	func changeLocation(to newLocation: String) {
+		locationName.value = "Loading..."
+		geocoder.geocode(addressString: newLocation) { [weak self] locations in
+			guard let self = self else { return }
+			if let location = locations.first {
+				self.locationName.value = location.name
+				self.fetchWeatherForLocation(location)
+				return
+			}
+			self.locationName.value = "Not found"
+			self.date.value = ""
+			self.icon.value = nil
+			self.summary.value = ""
+			self.forecastSummary.value = ""
+		}
+		self.locationName.value = "Not found"
+		self.date.value = ""
+		self.icon.value = nil
+		self.summary.value = ""
+		self.forecastSummary.value = ""
+	}
+	
+	private func fetchWeatherForLocation(_ location: Location) {
+		WeatherbitService.weatherDataForLocation(
+			latitude: location.latitude,
+			longitude: location.longitude) { [weak self] (weatherData, error) in
+				guard
+					let self = self,
+					let weatherData = weatherData
+					else {
+						return
+					}
+				self.date.value = self.dateFormatter.string(from: weatherData.date)
+				self.icon.value = UIImage(named: weatherData.iconName)
+				let temp = self.tempFormatter
+					.string(from: weatherData.currentTemp as NSNumber) ?? ""
+				self.summary.value = "\(weatherData.description) - \(temp)℉"
+				self.forecastSummary.value = "\nSummary: \(weatherData.description)"
+		}
+	}
+}
